6c64cc958a937fed7f2005d843444ac449ed3fe3
Greg Brown <gregplaysguitar@gmail.com>
2010-03-03 11:46:32 +1300

    linklist bug fixed - cheers matt

--- templates/djangocms2000/cms/linklist.js
+++ templates/djangocms2000/cms/linklist.js
@@ -1,5 +1,5 @@
 var tinyMCELinkList = [
     {% for page in page_list %}
-    ["{% firstof page.page_title page.get_absolute_url %}", "{{ page.get_absolute_url }}"],
+    ["{% firstof page.page_title page.get_absolute_url %}", "{{ page.get_absolute_url }}"]{% if not forloop.last %},{% endif %}
     {% endfor %}
 ];
