a04609a2ca738202de5818f9a4c4f315a7b428a3
Greg Brown <gregplaysguitar@gmail.com>
2010-06-03 15:55:32 +1200

    semi colon

--- templates/djangocms2000/cms/page_admin_init.js
+++ templates/djangocms2000/cms/page_admin_init.js
@@ -1,3 +1,3 @@
 $(function() {
     djangocms2000Admin("{{ djangocms2000_settings.FILEBROWSER_URL_ADMIN }}", "{% url djangocms2000.views.linklist %}", "{{ djangocms2000_settings.TINYMCE_BUTTONS }}", {% if request.user.is_superuser %}1{% else %}0{%endif %});
-})
+});
