180b264288ee2801b71d697e566b911513c2018d
Greg Brown <gregplaysguitar@gmail.com>
2011-03-09 11:18:11 +1300

    fixed a bug with cropping thuymbnails

--- templatetags/djangocms2000_tags.py
+++ templatetags/djangocms2000_tags.py
@@ -188,7 +188,10 @@ class CMSImageNode(template.Node):
             crop = template.Variable(self.crop).resolve(context)
         except:
             crop = self.crop
-            
+        
+        if crop == 'crop':
+            crop = 'center'
+        
         data = {
             'label': label,
             'request': context['request'],
