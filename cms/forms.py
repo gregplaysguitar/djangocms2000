e8db6dc84e2b807d20e77e468f140862f31630a1
Greg Brown <gregplaysguitar@gmail.com>
2012-08-02 12:56:27 +1200

    reinstated some admin stuff that got lost in the merge

--- cms/forms.py
+++ cms/forms.py
@@ -1,9 +1,26 @@
-from django import forms
-#from models import Block, Page, Image
-#from tinymce.widgets import TinyMCE
-from models import Page, template_choices
 import re
+
+from django import forms
 from django.conf import settings
+from django.core.urlresolvers import resolve, Resolver404
+from django.utils.safestring import mark_safe
+
+from models import Page, template_choices
+
+
+
+class ReadonlyInput(forms.widgets.HiddenInput):
+    is_hidden = False
+    def __init__(self, attrs=None, model=None):
+        super(ReadonlyInput, self).__init__(attrs)
+        self.model = model
+        
+    def render(self, name, value, attrs=None):
+        if self.model:
+            text_value = self.model.objects.get(pk=value)
+        else:
+            text_value = value
+        return mark_safe("<p>%s</p>%s" % (text_value, super(ReadonlyInput, self).render(name, value, attrs)))
 
 
 class BlockForm(forms.Form):
@@ -28,13 +45,30 @@ URL_DASH_REGEX = re.compile('--+')
 class PageForm(forms.ModelForm):
     template = forms.CharField(
         widget=forms.Select(choices=template_choices()),
-        help_text="Choose from Static Templates unless you're sure of what you're doing."
+        #help_text="Choose from Static Templates unless you're sure of what you're doing.",
+        required=False
     )
     def __init__(self, *args, **kwargs):
         super(PageForm, self).__init__(*args, **kwargs)
+        
         # just in case a template has been added/changed since last server restart
         self.fields['template'].widget.choices = template_choices()
-     
+                
+        # if the page is rendered by an actual django url, make template and url read-only
+        instance = kwargs.get('instance', None)
+        if instance:
+            try:
+                resolve(instance.url)
+            except Resolver404, e:
+                # must be an admin-created page, rendered by the middleware
+                pass
+            else:
+                # must be a django-created page, rendered by a urlconf
+                self.fields['url'].widget = ReadonlyInput()
+                self.fields['url'].help_text = ''
+                self.fields['template'].widget = ReadonlyInput()
+                #self.fields['template'].help_text = ''
+        
     class Meta:
         model = Page
     
