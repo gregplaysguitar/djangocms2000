c1407e4800429eea036e1d9b35966d26ce2eabd9
Greg Brown <gregplaysguitar@gmail.com>
2013-05-20 12:13:05 +1200

    url tag Django 1.5 compatibility

--- cms/templates/cms/cms/page_admin_init.js
+++ cms/templates/cms/cms/page_admin_init.js
@@ -1,8 +1,10 @@
+{% load url from future %}
+
 jQuery(function() {
     cmsAdmin(
         jQuery,
         "{{ cms_settings.FILEBROWSER_URL_ADMIN }}",
-        "{% url "cms.views.linklist" %}",
+        "{% url 'cms.views.linklist' %}",
         "{{ cms_settings.TINYMCE_CONTENT_CSS }}",
         "{{ cms_settings.TINYMCE_BUTTONS }}",
         {% if request.user.is_superuser %}1{% else %}0{%endif %}
