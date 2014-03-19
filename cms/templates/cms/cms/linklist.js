6ded44fd7e25fe866b16117d1a9dd859ec80ad7b
Greg Brown <gregplaysguitar@gmail.com>
2014-03-19 16:41:15 +1300

    linklist bug workaround

--- cms/templates/cms/cms/linklist.js
+++ cms/templates/cms/cms/linklist.js
@@ -1,6 +1,6 @@
 // TODO global var used by tinymce configs; should fix this
 var cms_tinymce_linklist = [
     {% for page in page_list %}
-    {title: "{{ page }}", value: "{{ page.get_absolute_url }}"}{% if not forloop.last %},{% endif %}
+    {title: "{{ page.get_absolute_url }}", value: "{{ page.get_absolute_url }}"}{% if not forloop.last %},{% endif %}
     {% endfor %}
 ];
