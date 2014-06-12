99ffdcea2e3f60de1accd61a29e63e726958ac00
Greg Brown <gregplaysguitar@gmail.com>
2014-06-13 11:01:46 +1200

    sort out some issues with admin page validation and append slash

--- cms/forms.py
+++ cms/forms.py
@@ -8,6 +8,17 @@ from django.utils.safestring import mark_safe
 from models import Page, Block, Image, template_choices
 import settings as cms_settings
 
+
+def url_resolves(url):
+    '''Test whether a url resolves successfully.'''
+    try:
+        resolve(url)
+    except Resolver404, e:
+        return False
+    else:
+        return True
+    
+
 class ReadonlyInput(forms.widgets.HiddenInput):
     is_hidden = False
     def __init__(self, attrs=None, model=None, display_text=None):
@@ -50,50 +61,33 @@ class PageForm(forms.ModelForm):
         # just in case a template has been added/changed since last server restart
         self.fields['template'].widget.choices = template_choices()
                 
-        # if the page is rendered by an actual django url, make template and url read-only
+        # if the page is rendered by an actual django url, make template 
+        # url and is_live read-only
         instance = kwargs.get('instance', None)
-        if instance:
-            try:
-                resolve(instance.url)
-            except Resolver404, e:
-                # must be an admin-created page, rendered by the middleware
-                pass
-            else:
-                # must be a django-created page, rendered by a urlconf
-                self.fields['url'].widget = ReadonlyInput()
-                self.fields['url'].help_text = ''
-
-                # TODO hide/delete these rather than showing them as readonly?
-                self.fields['template'].widget = ReadonlyInput(display_text='n/a')
-                self.fields['template'].help_text = ''
-                self.fields['is_live'].widget = ReadonlyInput(display_text='n/a')
-                self.fields['is_live'].help_text = ''
+        if instance and url_resolves(instance.url):
+            # must be a django-created page, rendered by a urlconf
+            self.fields['url'].widget = ReadonlyInput()
+            self.fields['url'].help_text = ''
+            
+            self.fields['template'].widget = ReadonlyInput(display_text='n/a')
+            self.fields['template'].help_text = ''
+            
+            self.fields['is_live'].widget = ReadonlyInput(display_text='n/a')
+            self.fields['is_live'].help_text = ''
         
     class Meta:
         model = Page
         exclude = ()
     
-    def clean(self):
+    def clean(self):        
         data = self.cleaned_data
-        if not data.get('template', None) and data.get('url', None):
-            # TODO: this may fail if creating a page for a different site with
-            # different urls
-            try:
-                resolve(data['url'])
-            except Resolver404, e:
-                err = 'This field is required for admin-created pages.'
-                self._errors['template'] = self.error_class([err])
         
-        # validate url/site uniqueness
+        # normalise url
         url = URL_STRIP_REGEX.sub('', data['url'].replace(' ', '-')).lower()
         url = URL_DASH_REGEX.sub('-', url).strip('-')
-        
         url = ("/%s" % (url.lstrip('/'))).replace('//', '/')
         
-        # WARNING: this assumes the APPEND_SLASH setting is common across sites
-        if settings.APPEND_SLASH and not url.endswith('/'):
-            url = "%s/" % url
-        
+        # check uniqueness of url/site
         if cms_settings.USE_SITES_FRAMEWORK:
             site_pages = Page.objects.filter(sites__in=data['sites'])
         else:
@@ -101,19 +95,34 @@ class PageForm(forms.ModelForm):
         
         if self.instance:
             site_pages = site_pages.exclude(pk=self.instance.pk)
-                
-        # for the purposes of uniqueness, test both slashed and non-slashed urls
+        
+        # for the purposes of uniqueness and testing the url, treat both 
+        # slashed and non-slashed urls as the same
         test_urls = [url.rstrip('/'), "%s/" % url.rstrip('/')]
+        
         clashes = site_pages.filter(url__in=test_urls)
         if clashes:
             sites = clashes.values_list('sites__domain', flat=True)
             err = 'A page with this url already exists for %s.' % ', '.join(sites)
             self._errors['url'] = self.error_class([err])
         
+        url_defined = any([url_resolves(u) for u in test_urls])
+        own_site = not data.get('sites') or (settings.SITE_ID in data['sites'])
+        
+        # The following validation only applies to pages created in the admin,
+        # and cannot be done reliably when editing a page for a different site
+        if own_site and not url_defined:
+            # Append a slash for consistency, but only if neither the original
+            # of the slash-appended url is defined in a urlconf
+            if settings.APPEND_SLASH and not url.endswith('/'):
+                url = url + '/'
+            
+            # Require template for admin-created pages
+            if not data.get('template'):
+                err = 'This field is required for admin-created pages.'
+                self._errors['template'] = self.error_class([err])
+        
         data['url'] = url
-        
-        #raise Exception
-        
         return data
         
 
--- cms/middleware.py
+++ cms/middleware.py
@@ -10,13 +10,17 @@ class CMSFallbackMiddleware(object):
             return response # No need to check for a page for non-404 responses.
         else:
             # append slash if required (only if the slash-appended url is valid)
-            if settings.APPEND_SLASH and not re.match(r'/$', request.path_info) and Page.objects.exclude(template='').filter(url="%s/" % request.path_info, sites__id=settings.SITE_ID).count():
+            if settings.APPEND_SLASH and not re.match(r'/$', request.path_info) \
+               and Page.objects.exclude(template='') \
+                               .filter(url="%s/" % request.path_info, 
+                                       sites__id=settings.SITE_ID).count():
                 return HttpResponseRedirect("%s/" % request.path_info)
             else:
                 try:
                     return render_page(request, request.path_info)
-                # Return the original response if any errors happened. Because this
-                # is a middleware, we can't assume the errors will be caught elsewhere.
+                # Return the original response if any errors happened. Because 
+                # this is a middleware, we can't assume the errors will be 
+                # caught elsewhere.
                 except Http404:
                     return response
                 except:
