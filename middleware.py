1a9c719ede2a0614d336529ec827336eaf7bea11
Greg Brown <gregplaysguitar@gmail.com>
2014-11-11 13:50:11 +1300

    uri sanity check

--- middleware.py
+++ middleware.py
@@ -6,12 +6,18 @@ from models import Page
 
 class Djangocms2000FallbackMiddleware(object):
     def process_response(self, request, response):
+        path = request.path_info
+        
+        # check uri sanity
+        if not re.match('^[\w\-\/]+$', path):
+            return response
+            
         if response.status_code != 404:
             return response # No need to check for a page for non-404 responses.
         else:
             # append slash if required (only if the slash-appended uri is valid)
-            if settings.APPEND_SLASH and not re.match(r'/$', request.path_info) and Page.objects.filter(uri="%s/" % request.path_info).count():
-                return HttpResponseRedirect("%s/" % request.path_info)
+            if settings.APPEND_SLASH and not re.match(r'/$', path) and Page.objects.filter(uri="%s/" % path).count():
+                return HttpResponseRedirect("%s/" % path)
             else:
                 try:
                     return render_page(request, request.path_info)
