b680fea81262f7e74a4f9672aa16e1d1eca740ee
Greg Brown <gregplaysguitar@gmail.com>
2015-04-16 21:46:38 +1200

    update setup and versioning

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-import cms
+#!/usr/bin/env python
+# coding: utf8
+
 from setuptools import setup, find_packages
 from setuptools.command.test import test
 
+# avoid importing the module 
+exec(open('cms/_version.py').read())
+
 setup(
     name='djangocms2000',
-    version=cms.__version__,
+    version=__version__,
     description='Flexible Django CMS with edit-in-place capability',
     long_description=open('readme.markdown').read(),
     author='Greg Brown',
     author_email='greg@gregbrown.co.nz',
     url='https://github.com/gregplaysguitar/djangocms2000',
-    packages=['cms'],
-    platforms='any',
+    packages=find_packages(),
     zip_safe=False,
+    platforms='any',
+    install_requires=['Django>=1.4',],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
