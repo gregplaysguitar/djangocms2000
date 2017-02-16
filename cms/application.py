f11304e92aeb3e76f5cc5eb977bf4af922db7122
Greg Brown <greg@gregbrown.co.nz>
2017-02-17 10:57:17 +1300

    Update application.py

--- cms/application.py
+++ cms/application.py
@@ -35,8 +35,8 @@ def thumbnail(image, geometry, crop, bw):
             'bw': bw,
         })
     else:
-        return sorl.thumbnail.get_thumbnail(image, 'x'.join([str(g) for g in geometry]), crop,
-                                            colorspace='GRAY' if bw else None)
+        return sorl.thumbnail.get_thumbnail(image, 'x'.join([str(g) for g in geometry]), crop=crop,
+                                            colorspace=('GRAY' if bw else None))
 
 
 def is_language_aware(model_cls):
